dcf732425cff0d743dbc6be57cfbf612e722e006
niko391a <nals@itu.dk>
2024-03-14 22:22:08 +0100

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ import javax.security.auth.login.LoginException;
 
 public class Main {
     public static void main(String[] args) throws LoginException {
-        final String TOKEN = "";
+        final String TOKEN = ""; // Token should be into an enviroment variable as it is not wise to leave token in source code.
         JDABuilder jdaBuilder = JDABuilder.createDefault(TOKEN);
 
         jdaBuilder.build();
